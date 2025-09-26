acde5de0159bf34d4d184f1f01c4aaf99327f384
pz520 <3520936145@qq.com>
2025-09-26 11:52:35 +0800

    Add files via upload

---
+++ string01.java
@@ -0,0 +1,26 @@
+public class string01 {
+
+public static void main(String[] args){
+int n1 =1;
+float f1 =2.2f;
+double d1=1.1;
+boolean b1 =true;
+String s1 =n1+"";
+String s2 =f1+"";
+String s3 =d1+"";
+String s4 =b1+"";
+System.out.println(s1+" "+s2+" "+s3+" "+s4);
+//System.out.println(s2+"");
+//System.out.println(s3+"");
+//System.out.println(s4+"");
+}
+
+
+}
+class String02{
+public static void main(String[] args){
+    String s1 ="123";
+    int num1 = Integer.parseInt(s1);
+    System.out.println(num1);
+}
+}
